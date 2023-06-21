2bc667c198939151cc14c04a1d0c3924fb44eaa4
apollo110217 <popescufv1021@gmail.com>
2023-06-21 10:51:33 +0200

    add: new custom hook

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# useWindowSize-react-hook" 
+"# useWindowSize-react-hook"
+Use the useState() hook to initialize a state variable that will hold the window's dimensions. Initialize with both values set to undefined to avoid mismatch between server and client renders.
+Create a function that uses Window.innerWidth and Window.innerHeight to update the state variable.
+Use the useEffect() hook to set an appropriate listener for the 'resize' event on mount and clean it up when unmounting.
---
+++ index.tsx
@@ -0,0 +1,32 @@
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = React.useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  React.useEffect(() => {
+    const handleResize = () =>
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return windowSize;
+};
+const MyApp = () => {
+  const { width, height } = useWindowSize();
+
+  return (
+    <p>
+      Window size: ({width} x {height})
+    </p>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<MyApp />);
